13e824f7b9d5118e3a96d08330b39da9d6a2bd28
Thales Alves <thalesalves758@gmail.com>
2022-06-24 01:10:58 -0300

    Import database and participantController modules and use them

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import database from './database/db.js';
+
+import participantsRoutes from './src/routes/participantsRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/participants', participantsRoutes);
+
 app.listen(process.env.PORT, () => {
+  database.initMongoClient();
+
   console.log(`Server is running on port ${process.env.PORT}`);
 });
