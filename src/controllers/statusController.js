7bacdecdc324f9d1c27fa12062edc337fdefa869
Thales Alves <thalesalves758@gmail.com>
2022-06-24 17:37:58 -0300

    Check if user exists

--- src/controllers/statusController.js
+++ src/controllers/statusController.js
@@ -1,6 +1,23 @@
+import database from '../../database/db.js';
+import httpStatus from '../utils/httpStatus.js';
+
 export default {
   post: async (req, res) => {
     const user = req.get('user');
-    res.send(user);
+
+    try {
+      const connection = await database.connectDatabase();
+
+      const participantsCollection = connection.collection('participants');
+      const participant = await participantsCollection.findOne({ name: user });
+
+      if (!participant) {
+        res.sendStatus(httpStatus.NOT_FOUND);
+        return;
+      }
+      res.send('ok');
+    } catch (e) {
+      res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+    }
   },
 };
