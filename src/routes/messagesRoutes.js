caa2b67a6b5095f3f38d3fb25c09d3df4ebb99f9
Thales Alves <thalesalves758@gmail.com>
2022-06-25 17:37:31 -0300

    Create PUT /:messageId route

--- src/routes/messagesRoutes.js
+++ src/routes/messagesRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/', messageController.get);
 router.post('/', messageController.post);
 router.delete('/:messageId', messageController.delete);
+router.put('/:messageId', messageController.put);
 
 export default router;
